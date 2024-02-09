3a8ca98b2bfdcc1cd6d98222a7aa679129c49712
IvoCorrea <ivoaurelio30@gmail.com>
2024-02-09 12:36:54 -0300

    Criaçao da rota post com integraçao de db

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,24 @@
 import fastify from "fastify"
-const app = fastify()
+import { z } from "zod"
+import { PrismaClient } from "@prisma/client";
+import { table } from "console";
 
-app.get('/hello', async (request, reply) => {
-    reply.send('hello')
+const app = fastify()
+const prisma = new PrismaClient
+
+app.post('/polls', async (request, reply) => {
+    const creatPollBody = z.object({
+        title: z.string()
+    })
+    const { title } = creatPollBody.parse(request.body)
+
+    const poll = await prisma.poll.create({
+        data: {
+            title,
+        }
+    })
+    
+    return reply.status(201).send({pollId: poll.id})
 })
 
 app.listen({ port:3333 }).then(() => {
